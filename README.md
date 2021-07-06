9dfafa7855fdede17760cb330058f6bc0370280a
regulad <52430642+regulad@users.noreply.github.com>
2021-07-05 20:21:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Matchmaker
+
+Matchmaker is a BungeeCord plugin used to make matches on [ender.quest](https://www.ender.quest).
+
+
---
+++ src/main/java/quest/ender/Matchmaker/Matchmaker.java
@@ -0,0 +1,6 @@
+package quest.ender.Matchmaker;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Matchmaker extends Plugin {
+}
